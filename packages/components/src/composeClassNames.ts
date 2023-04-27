7eabe5e05ffb44bffd20fc2d8e4f2571dc18baa2
Marin Aeschbach <marin.aeschbach@gmail.com>
2023-04-27 14:20:46 +0200

    Add less generic classname composition func, no longer export it

--- packages/components/src/composeClassNames.ts
+++ packages/components/src/composeClassNames.ts
@@ -1,11 +1,5 @@
 export function composeClassNames(
   ...classNames: (string | null | undefined)[]
 ) {
-  const classes = classNames.filter((className) => {
-    if (typeof className === 'string') {
-      className = className.trim();
-    }
-    return className;
-  });
-  return classes.length === 0 ? undefined : classes.join(' ');
+  return classNames.filter((className) => className).join(' ');
 }
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,12 +1,6 @@
-import { composeClassNames } from './composeClassNames';
 import { createBaseComponent, BaseComponentProps } from './createBaseComponent';
 import { createComponent, ComponentProps } from './createComponent';
 import { extractAtomsFromProps } from './extractAtomsFromProps';
 
-export {
-  composeClassNames,
-  createBaseComponent,
-  createComponent,
-  extractAtomsFromProps,
-};
+export { createBaseComponent, createComponent, extractAtomsFromProps };
 export type { BaseComponentProps, ComponentProps };
