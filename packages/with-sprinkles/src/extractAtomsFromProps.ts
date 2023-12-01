438c742943f57d33e3555ec560d02d60ef088402
Marin Aeschbach <marin.aeschbach@gmail.com>
2023-12-01 10:37:05 +0100

    Rename type params

--- packages/with-sprinkles/src/extractAtomsFromProps.ts
+++ packages/with-sprinkles/src/extractAtomsFromProps.ts
@@ -1,17 +1,18 @@
-import { type SprinklesFnBase } from './SprinklesFnBase';
+import type { SprinklesFnBase } from '@muffin-tin/core';
 
 export function extractAtomsFromProps<
-  NSV extends Record<string, unknown>,
-  SV extends Record<string, unknown>,
->(props: SV & NSV, sprinklesFns: SprinklesFnBase[]) {
-  const sprinkleProps = {} as SV;
-  const otherProps = {} as NSV;
+  TOtherProps extends Record<string, any>,
+  TSprinklesProps extends Record<string, any>,
+>(props: TSprinklesProps & TOtherProps, sprinklesFns: SprinklesFnBase[]) {
+  const sprinkleProps = {} as TSprinklesProps;
+  const otherProps = {} as TOtherProps;
 
   for (const key in props) {
     if (sprinklesFns.some((sprinkleFn) => sprinkleFn.properties.has(key))) {
-      sprinkleProps[key as keyof SV] = props[key as keyof SV];
+      sprinkleProps[key as keyof TSprinklesProps] =
+        props[key as keyof TSprinklesProps];
     } else {
-      otherProps[key as keyof NSV] = props[key as keyof NSV];
+      otherProps[key as keyof TOtherProps] = props[key as keyof TOtherProps];
     }
   }
 
